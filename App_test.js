fdf7be964bc3771dcfca2b6962407189f6acd57a
Vigneshwar <prasannavenkatesh@colanonline.com>
2022-02-18 10:22:50 +0530

    project push initially

---
+++ App_test.js
@@ -0,0 +1,14 @@
+import React, { Component, useEffect, useState } from 'react';
+
+import {View, Text} from 'react-native'
+const ForTest = () => {
+    return (
+        <View>
+            <Text>
+Hello world
+            </Text>
+        </View>
+    );
+}
+
+export default ForTest;
---
+++ ios/Pods/Target Support Files/Pods-my_rn_app-my_rn_appTests/Pods-my_rn_app-my_rn_appTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_my_rn_app_my_rn_appTests : NSObject
+@end
+@implementation PodsDummy_Pods_my_rn_app_my_rn_appTests
+@end
